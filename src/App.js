e577491fc6d9b9ce58b4070d1f9b276150bde532
dev-phil <piljun@pplink.net>
2020-06-25 20:12:37 +0900

    Add and Delete Items

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
 
 
 function App() {
-  const [hello,setHello] = useState('Hello React World');
-  const onCick = () => {
-    setHello('Have changed characters')
-  }
+  const [list,setList] = useState([]);
+  const [text,setText] = useState('');
+  const submit = () => {
+    setList([...list,text]);
+    setText('');
+  };
+
+  const removeItem = (index) => {
+    const resultArray = list.filter((item,idx)=> {
+      if(idx === index) { 
+        return false
+      }
+        return true;
+    });
+      setList(resultArray);
+  };
+
   return (
-    <div className="App">
-      {hello}
-      <button onClick={onCick}>Changing Characters</button>
-    </div>
+    <StyledApp className="App">
+      <div className='list'>
+      </div>
+
+      <div>
+        <div>
+          {list.map((item, idx)=>{
+            return <div>
+              <div>
+                {item}
+              </div>
+              <button onClick={()=>removeItem(idx)}>
+                Delete
+              </button>
+            </div>
+          })}
+        </div>
+
+
+        <input value={text} onChange={(e)=> {
+          setText(e.target.value);
+        }}/>
+        <button onClick={submit}> Add </button>
+      </div>
+
+
+    </StyledApp>
   );
 }
+
+const StyledApp  = styled.div`
+  background-color : skyblue;
+`;
 
 export default App;
